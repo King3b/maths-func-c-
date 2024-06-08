35bdd2c47e0a2ef31657958cf64b563231c260fc
King4one <101467714+King3b@users.noreply.github.com>
2024-06-08 22:53:56 +0200

    Add files via upload

---
+++ Calculations.cs
@@ -0,0 +1,92 @@
+﻿namespace maths_Fun;
+public static class Question2
+{
+
+    public static double Arithmetic(double a, double d, double n)
+    {
+        double Tn = a + d * (n - 1); return Tn;
+    }
+    public static double ArithmeticTotal(double a, double d, double n, double l)
+    {
+        if (l == 0)
+        {
+            double Sn = (n / 2) * ((2 * a) + d * (n - 1)); return Sn;
+        }
+        else { double Sn = (n * (a + l)) / 2; return Sn; }
+
+    }
+    public static double Geometric(double a, double r, double n)
+    {
+        double Tn = a * Math.Pow(r, (n - 1)); return Tn;
+    }
+    public static double GeometricTotal(double a, double r, double n)
+    {
+        if (r > 1)
+        {
+            double Sn = (a * (Math.Pow(r, n) - 1)); return Sn;
+        }
+        else
+        {
+            double Sn = (a * (1 - Math.Pow(r, n))); return Sn;
+        }
+    }
+    public static double infinity(double a, double r)
+    {
+        double Sn = a / (1 - r); return Sn;
+    }
+    public static double SigmaNotation(double n, double k, double p)
+    {
+        double Sn = n - k + 1; return Sn;
+    }
+    public static double SigmaNotationPattern(double n, double k, double p)
+    {
+        double Sn = n - k + 1;
+        double total = 0;
+        for (int i = 0; i < Sn; i++)
+        {
+            total = total + p;
+        }
+        return total;
+    }
+
+}
+
+public static class Quesion3
+{
+    public static double parabola(double a, double x, double p, double q)
+    {
+        double y = a * Math.Pow((x - p), 2) + q; return y;
+    }
+    public static double parabolaForm2(double a, double b, double c, double x)
+    {
+        double y = (a * Math.Pow(x, 2) + (b * x) + c); return y;
+    }
+    public static double ParabolaTurningPoint(double a, double b, double p, double q)
+    {
+        double y = (-b) / (2 * a); return y;
+    }
+
+    public static double Hyperbole(double a, double x, double p, double q)
+    {
+        double y = (a / (x - p)) + q; return y;
+    }
+    public static double HyperboleSymetry(double a, double x, double p, double q)
+    {
+        if (a > 0)
+        {
+            double y = x - p + q; return y;
+        }
+        else
+        {
+            double y = -x + p + q; return y;
+        }
+    }
+    public static double Exponetial(double a, double x, double p, double b, double q)
+    {
+        double y = a * Math.Pow(b, (x - p) + q); return y;
+    }
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] arg)
+        {
+            int Question;
+            Console.WriteLine("which question ");
+            Question = Convert.ToInt32(Console.ReadLine());
+            if (Question == 2)
+            {
+
+            
+            // variables
+            double a,d,n,r,l,k,p;
+            int question;
+            // selection
+            Console.WriteLine("1 = Arithmetic "       + " 5 = Infinity ");
+            Console.WriteLine("2 = arithmetic Total " + "6 = Sigma Notation");
+            Console.WriteLine("3 = geometric "         + " 7 = Sigma Notation Pattern");
+            Console.WriteLine("4 = Geometric total ");
+            Console.WriteLine("enter the sum number");
+            question = Convert.ToInt32(Console.ReadLine());
+          
+            if (question == 1 )
+            {
+                Console.WriteLine(" enter a");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter d");
+                d = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                    // method
+
+                    double anw = maths_Fun.Question2.Arithmetic(a,d,n);
+                Console.WriteLine("the anw is "+ anw);
+            }
+            else if (question == 2)
+            {
+                Console.WriteLine(" enter a");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter d");
+                d = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter l");
+                l = Convert.ToDouble(Console.ReadLine());
+
+                // method
+                double anw = maths_Fun.Question2.ArithmeticTotal(a,d,n,l);
+                Console.WriteLine("the anw is " + anw);
+            }
+            else if (question == 3)
+            {
+                Console.WriteLine(" enter a");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter r");
+                r = Convert.ToDouble(Console.ReadLine());
+
+                // method
+                double anw = maths_Fun.Question2.Geometric(a,r,n);
+                Console.WriteLine("the anw is " + anw);
+            }
+            else if (question == 4)
+            {
+                Console.WriteLine(" enter a");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter r");
+                r = Convert.ToDouble(Console.ReadLine());
+
+                // method
+                double anw = maths_Fun.Question2.GeometricTotal(a,r,n);
+                Console.WriteLine("the anw is " + anw);
+            }
+            else if (question == 5)
+            {
+                Console.WriteLine(" enter a");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter r");
+                r = Convert.ToDouble(Console.ReadLine());
+
+                // method
+                double anw = maths_Fun.Question2.infinity(a, r);
+                Console.WriteLine("the anw is " + anw);
+            }
+            else if (question == 6)
+            {
+
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter k");
+                k = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter p");
+                p = Convert.ToDouble(Console.ReadLine());
+
+                //method
+                double anw = maths_Fun.Question2.SigmaNotation(n, k, p);
+                Console.WriteLine("the anw is " + anw);
+            }
+            else if (question == 7)
+            {
+                Console.WriteLine(" enter n");
+                n = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter k");
+                k = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" enter p");
+                p = Convert.ToDouble(Console.ReadLine());
+
+                //method
+                double anw = maths_Fun.Question2.SigmaNotationPattern(n, k, p);
+                Console.WriteLine("the anw is " + anw);
+            }
+             
+
+            Console.Read(); 
+        }
+    }
+
+       
+
+    }
+}
